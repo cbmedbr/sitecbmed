3da364ba84c41d8412bf8aa349163d67d82b2f89
cbmedbr <info@cbmed.com.br>
2026-05-15 15:17:23 -0300

    fix(science): molécula alinha no topo da coluna com pt-8

--- components/ScienceSection.js
+++ components/ScienceSection.js
@@ -86,7 +86,7 @@ export default function ScienceSection() {
           </div>
 
           {/* Coluna direita: molécula CBD interativa */}
-          <div className="flex items-center justify-center h-full">
+          <div className="flex items-start justify-center h-full pt-8">
             <MoleculeCBD size={320} />
           </div>
 
